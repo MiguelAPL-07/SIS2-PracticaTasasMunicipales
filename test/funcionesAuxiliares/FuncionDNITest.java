7d2c828e505c88d7f36f1fb9018e7615cfa46f10
MiguelAPL-07 <miguelangelpl2020@gmail.com>
2024-04-17 02:13:50 +0200

    Actualizando y agregando mas test funcion DNI

--- test/funcionesAuxiliares/FuncionDNITest.java
+++ test/funcionesAuxiliares/FuncionDNITest.java
@@ -38,7 +38,7 @@ public class FuncionDNITest {
         assertEquals(1, fDNI.validadorNIF_NIE("14754857L"));
         // DNI incorrecto pero subsanable y duplicado
         assertEquals(3, fDNI.validadorNIF_NIE("12345678A"));
-        // DNI subsanable pero duplicado
+        // DNI incorrecto pero subsanable y duplicado
         assertEquals(3, fDNI.validadorNIF_NIE("12345678"));
     }
     
@@ -51,11 +51,15 @@ public class FuncionDNITest {
         assertEquals(0, fDNI.validadorNIF_NIE("Z1174494G"));
         // NIE incorrecto
         assertEquals(2, fDNI.validadorNIF_NIE("X053450"));
-        assertEquals(2, fDNI.validadorNIF_NIE("X0134567A"));
+        assertEquals(2, fDNI.validadorNIF_NIE("X0134567AA"));
         // NIE incorrecto pero subsanable, letra incorrecta Y9679641J
         assertEquals(1, fDNI.validadorNIF_NIE("Y9679641M"));
         // NIE incorrecto pero subsanable, sin letra
         assertEquals(1, fDNI.validadorNIF_NIE("Y1234567"));
+        // NIE incorrecto pero subsanable y duplicado
+        assertEquals(3, fDNI.validadorNIF_NIE("X9339365"));
+        // NIE incorrecto pero subsanable y duplicado
+        assertEquals(3, fDNI.validadorNIF_NIE("Y4417420L"));
     }
     
     @Test
@@ -65,6 +69,15 @@ public class FuncionDNITest {
         assertEquals("12345678Z", fDNI.corregirDocumento("12345678"));
         // Letra incorrecta
         assertEquals("12345678Z", fDNI.corregirDocumento("12345678B"));
+        // Sin letra NIE
+        assertEquals("X9339365P", fDNI.corregirDocumento("X9339365"));
+        assertEquals("Y4417420P", fDNI.corregirDocumento("Y4417420"));
+        assertEquals("Z1174494G", fDNI.corregirDocumento("Z1174494"));
+        // Letra incorrecta NIE
+        assertEquals("Z6577493G", fDNI.corregirDocumento("Z6577493P"));
+        assertEquals("Y2397509A", fDNI.corregirDocumento("Y2397509L"));
+        assertEquals("X6516217H", fDNI.corregirDocumento("X6516217B"));
+        
     }
     
     @Test
@@ -83,5 +96,23 @@ public class FuncionDNITest {
         String docActualizado = fDNI.corregirDocumento("56726023");
         assertEquals("56726023L", docActualizado);
         assertEquals(true, fDNI.comprobarDocumentoDuplicado(docActualizado));
+        
+        // Comprobar NIE duplicado
+        // Validamos varios y ya se agregan al array
+        assertEquals(0, fDNI.validadorNIF_NIE("Z6577493G"));
+        assertEquals(0, fDNI.validadorNIF_NIE("Y2397509A"));
+        assertEquals(0, fDNI.validadorNIF_NIE("X6516217H"));   
+        assertEquals(0, fDNI.validadorNIF_NIE("X9339365P"));
+        assertEquals(0, fDNI.validadorNIF_NIE("Y4417420P"));
+        assertEquals(0, fDNI.validadorNIF_NIE("Z1174494G"));   
+        // NIE duplicado en el array
+        assertEquals(false, fDNI.comprobarDocumentoDuplicado("Z1958640B"));  
+        assertEquals(true, fDNI.comprobarDocumentoDuplicado("X9339365P")); 
+        
+        // NIE subsanable pero duplicado
+        assertEquals(3, fDNI.validadorNIF_NIE("Z6577493"));
+        String docActualizado2 = fDNI.corregirDocumento("Z6577493");
+        assertEquals("Z6577493G", docActualizado2);
+        assertEquals(true, fDNI.comprobarDocumentoDuplicado(docActualizado2));
     }
 }
