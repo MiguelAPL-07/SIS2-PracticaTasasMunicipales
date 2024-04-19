c0d8ea693ae428810985a924db989f4771a7a92c
MiguelAPL-07 <miguelangelpl2020@gmail.com>
2024-04-19 14:01:53 +0200

    Integrando todas las funciones creadas

--- src/Practicas/PracticaDos.java
+++ src/Practicas/PracticaDos.java
@@ -7,7 +7,10 @@ package Practicas;
 
 
 import funcionesAuxiliares.Constantes;
+import funcionesAuxiliares.FuncionCCC;
 import funcionesAuxiliares.FuncionDNI;
+import funcionesAuxiliares.FuncionEmail;
+import funcionesAuxiliares.FuncionIBAN;
 import java.io.File;
 import java.util.HashMap;
 import manager.ExcelManager;
@@ -42,7 +45,7 @@ public class PracticaDos {
     
     public void ejecucion() {
         primeraEjecucionDNI();
-        
+        segundaEjecucionCCC();
     }
 
     private void primeraEjecucionDNI() {
@@ -79,10 +82,50 @@ public class PracticaDos {
     }
     
     private void segundaEjecucionCCC() {
+        // Actualizo la lista de contribuyentes con las actualizaciones
+        contribuyentes = em.obtenerContribuyentes();
+        FuncionDNI fDNI = new FuncionDNI();
+        FuncionEmail fEmail = new FuncionEmail();
+        FuncionCCC fCCC = new FuncionCCC();
+        FuncionIBAN fIBAN = new FuncionIBAN();
+        XmlManager xmlManager = new XmlManager();
         
-    }
-    
-    private void terceraEjecucionIbanEmail() {
+        for(int i = 1; i <= em.getUltimaFilaContribuyentes(); i++) {
+            
+            if(contribuyentes.containsKey(String.valueOf(i))) {
+                ContribuyenteExcel c = (ContribuyenteExcel) contribuyentes.get(String.valueOf(i));
+                
+                String iban = "";
+                // Verifica si el codigo de cuenta es correcto
+                if(fCCC.verificarCCC(c.getCcc())) {
+                    // Valido DNI. Si es incorrecto no hago nada
+                    if(fDNI.validadorNIF_NIE(c.getNifnie()) == 0) {
+                        // Calcula IBAN y agrega al Excel
+                        iban = fIBAN.calculoIBAN(c.getPaisCCC(), c.getCcc());
+                        em.agregarIBANHoja(iban, i);
+                        // Genera el email y agrega al Excel
+                        String email = fEmail.generarEmail(c.getNombre(), c.getApellido1(), c.getApellido2());
+                        em.agregarEmailHoja(email, i);
+                    }
+                } else {
+                    // Corregir el ccc, generar el IBAN, agregar al Excel y enviar al XML
+                    String cccCorregido = fCCC.corregirCCC(c.getCcc());
+                    iban = fIBAN.calculoIBAN(c.getPaisCCC(), cccCorregido);
+                    xmlManager.agregarNodoDocumentoErroresCCC(i, c.getNombre(), 
+                            c.getApellido1() + " " + c.getApellido2(), 
+                            c.getNifnie(), c.getCcc(), iban);
+                    if(fDNI.validadorNIF_NIE(c.getNifnie()) == 0) {
+                        em.modificarCCCHoja(cccCorregido, i);
+                        em.agregarIBANHoja(iban, i);
+                        // Genera el email y agrega al Excel
+                        String email = fEmail.generarEmail(c.getNombre(), c.getApellido1(), c.getApellido2());
+                        em.agregarEmailHoja(email, i);
+                    }
+                }   
+            }
+        }
         
+        // Se genera el xml
+        xmlManager.generarDocumentoXmlErroresCCC();
     }
 }
